8deb9b2258f4985151ed52cfac89d350e5dbcd51
Vuk Vicentic <vuk.vicentic@gmail.com>
2025-05-12 17:01:18 +0200

    add backend

---
+++ Backend/demo/src/main/java/com/example/demo/FitnessManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessManagerApplication.class, args);
+	}
+
+}
