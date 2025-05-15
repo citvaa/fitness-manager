038ca61b2558a8d510be5127b2fc66474dea7e86
Vuk Vicentic <vuk.vicentic@gmail.com>
2025-05-15 16:25:38 +0200

    feat: add flyway script for admin insertion

---
+++ Backend/demo/src/main/resources/db/migration/V1.0002__add_admin_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user" (id, username, password, email, registration_key, registration_key_validity, is_activated, reset_token, reset_token_validity, version)
+VALUES
+    (0, 'admin', '$2a$10$sBgQPoZZJkbMf5usoTFTIuHvkOgqO2bigK2okAB0XUli/0sq.IdU6', 'admin@gmail.com', null, null, true, null, null, 1);
