556bc45b63cb0e16b3d67fa8b511fe3706403567
Aleksandar Vicentic <aleksandar.vicentic@enetelsolutions.com>
2025-05-15 17:49:28 +0200

    #6 minor fix.

--- Backend/demo/src/main/resources/db/migration/V1.0002__add_admin_user.sql
+++ Backend/demo/src/main/resources/db/migration/V1.0002__add_admin_user.sql
@@ -1,3 +1,3 @@
-INSERT INTO "user" (id, username, password, email, registration_key, registration_key_validity, is_activated, reset_token, reset_token_validity, version)
+INSERT INTO "user" (username, password, email, registration_key, registration_key_validity, is_activated, reset_key, reset_key_validity, version)
 VALUES
-    (0, 'admin', '$2a$10$sBgQPoZZJkbMf5usoTFTIuHvkOgqO2bigK2okAB0XUli/0sq.IdU6', 'admin@gmail.com', null, null, true, null, null, 1);
+    ('admin', '$2a$10$sBgQPoZZJkbMf5usoTFTIuHvkOgqO2bigK2okAB0XUli/0sq.IdU6', 'admin@gmail.com', null, null, true, null, null, 1);
